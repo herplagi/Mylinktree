0b27bb56e489f268e3c45ce5a64b609200cce868
herplagi <alvinoalbas@gmail.com>
2026-07-13 15:48:37 +0700

    Update base URL in Vite configuration to match new project name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Pinotlinktree/",
+  base: "/Mylinktree/",
   plugins: [react(), tailwindcss()],
 });
